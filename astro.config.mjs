b5c06d321ced7e1f662fa7041673fcb55e2f5d51
Jok-Holk <jokholk.dev@gmail.com>
2025-09-04 19:52:18 +0700

    reset vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,18 +7,5 @@ export default defineConfig({
     integrations: [vue(), tailwind()],
     site: 'https://jokholk.dev',
     output: 'static',
-    adapter: vercel({
-        redirects: [
-            {
-                source: '/en',
-                destination: '/',
-                permanent: true,
-            },
-            {
-                source: '/en/:path*',
-                destination: '/:path*',
-                permanent: true,
-            },
-        ],
-    }),
+    adapter: vercel(),
 });
