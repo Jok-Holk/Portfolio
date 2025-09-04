a5957b83065fe47a1aa71dbf50ed36e8dea7d492
Jok-Holk <jokholk.dev@gmail.com>
2025-09-04 19:31:16 +0700

    remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,9 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    integrations: [
-        vue(),
-        tailwind(),
-        sitemap({
-            filter: (page) => page === '/' || page === '/en/' || page === '/vi/',
-        }),
-    ],
+    integrations: [vue(), tailwind()],
     site: 'https://jokholk.dev',
     output: 'static',
     adapter: vercel(),
