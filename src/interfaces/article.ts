777e16af80530e4b073d946f4b785121f8a81221
Jok-Holk <jokholk.dev@gmail.com>
2025-09-07 21:47:26 +0700

    update

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -2,6 +2,7 @@ export default interface Article {
     id: number;
     documentId: string;
     title: string;
+    description: string;
     content: string;
     publish_date: string;
     slug: string;
