7c74948a666ad4925df7adf7d6fc535a57552add
Jok-Holk <jokholk.dev@gmail.com>
2025-09-16 12:50:36 +0700

    add 404 site

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
                     vi: 'vi-VN',
                 },
             },
+            filter: (page) => !page.includes('/404'),
         }),
     ],
     site: 'https://jokholk.dev',
