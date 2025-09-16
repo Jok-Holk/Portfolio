8d6c3ea83379105cca24a084c5402f8c5b44fa5c
Jok-Holk <jokholk.dev@gmail.com>
2025-09-16 12:25:54 +0700

    update test sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,22 @@ import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    integrations: [vue(), tailwind()],
+    integrations: [
+        vue(),
+        tailwind(),
+        sitemap({
+            i18n: {
+                defaultLocale: 'en',
+                locales: {
+                    en: 'en-US',
+                    vi: 'vi-VN',
+                },
+            },
+        }),
+    ],
     site: 'https://jokholk.dev',
     output: 'static',
     adapter: vercel(),
