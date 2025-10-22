aaaf35b819d7daa749cd354addfdabb56478a59f
Jok-Holk <jokholk.dev@gmail.com>
2025-10-22 16:29:17 +0700

    test

--- src/pages/api/download-cv/[lang].js
+++ src/pages/api/download-cv/[lang].js
@@ -2,12 +2,16 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 export async function GET({ params }) {
-    const filePath = join(process.cwd(), 'public', 'resumes', params.lang, `resume-${params.lang}.pdf`);
-    const fileBuffer = readFileSync(filePath);
-    return new Response(fileBuffer, {
-        headers: {
-            'Content-Type': 'application/pdf',
-            'Content-Disposition': 'attachment; filename="resume.pdf"',
-        },
-    });
+    try {
+        const filePath = join(process.cwd(), 'public', 'resumes', params.lang, `resume-${params.lang}.pdf`);
+        const fileBuffer = readFileSync(filePath);
+        return new Response(fileBuffer, {
+            headers: {
+                'Content-Type': 'application/pdf',
+                'Content-Disposition': 'attachment; filename="resume.pdf"',
+            },
+        });
+    } catch (error) {
+        return new Response('PDF not found', { status: 404 });
+    }
 }
